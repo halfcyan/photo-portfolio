a8d1f6c119d4e5131aa8ac694b8230d5b2bbec80
halfcyan <nicholasreed04@gmail.com>
2024-04-08 14:56:41 -0500

    add firebase and secret environment variable

---
+++ setEnv.sh
@@ -0,0 +1,2 @@
+mkdir env
+echo "export const api_key = { api_key : '$1' }"  > env/env.js
---
+++ src/config.js
@@ -0,0 +1,6 @@
+import { api_key as API} from './env/env.js'
+
+export default {
+    api_key: API,
+    photoset: "72177720316091768",
+}
